1ef0e4a216d2996c568fbf1b13b77b259b49e79c
cevikbasesra <cevikbasesra@gmail.com>
2024-12-13 16:58:35 +0300

    created home page

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+// HomePage.js
+import React from "react";
+import { Link } from "react-router-dom";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to the E-Commerce Website</h1>
+      <p>This is the homepage.</p>
+    </div>
+  );
+};
+
+export default HomePage;
