0c05d0fd8c07089ccd6d5df40a16c66bf750abd5
cevikbasesra <cevikbasesra@gmail.com>
2024-12-19 00:17:42 +0300

    for maintain consistency between reducer and action

---
+++ src/actions/types/authTypes.js
@@ -0,0 +1,15 @@
+// Authentication
+export const AUTH_START = 'AUTH_START';
+export const AUTH_SUCCESS = 'AUTH_SUCCESS';
+export const AUTH_FAIL = 'AUTH_FAIL';
+export const LOGOUT = 'LOGOUT';
+
+// Registration
+export const REGISTER_START = 'REGISTER_START';
+export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
+export const REGISTER_FAIL = 'REGISTER_FAIL';
+
+// Token
+export const TOKEN_REFRESH_START = 'TOKEN_REFRESH_START';
+export const TOKEN_REFRESH_SUCCESS = 'TOKEN_REFRESH_SUCCESS';
+export const TOKEN_REFRESH_FAIL = 'TOKEN_REFRESH_FAIL';
