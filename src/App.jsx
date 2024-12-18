d88c4403c2f99bca5080daec023ac8da570e44be
cevikbasesra <cevikbasesra@gmail.com>
2024-12-19 00:26:56 +0300

    updated both file

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,39 @@
 // App.js
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Header from "./layout/Header"; // Header bileşenini import et
-import LoginPage from "./pages/LoginPage"; // LoginPage bileşenini import et
-import SignupPage from "./pages/SignupPage"; // SignupPage bileşenini import et
-import HomePage from "./pages/HomePage"; // HomePage bileşenini import et
-import { ToastContainer } from "react-toastify"; // Toastify için
-import "react-toastify/dist/ReactToastify.css"; // Toastify CSS
+import Header from "./layout/Header";
+import Footer from "./layout/Footer";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import HomePage from "./pages/HomePage";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   return (
     <Router>
-      <Header /> {/* Header bileşenini burada kullanıyoruz */}
-      <div className="container mx-auto p-4">
-        <Routes>
-          <Route path="/" element={<HomePage />} /> {/* HomePage için rota */}
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignupPage />} />
-          {/* Diğer sayfalar buraya eklenebilir */}
-        </Routes>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-grow container mx-auto p-4">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignupPage />} />
+          </Routes>
+        </main>
+        <Footer />
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={true}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
       </div>
-      <ToastContainer
-        position="top-right"
-        autoClose={5000} // Mesajların 5 saniye sonra kapanması
-        hideProgressBar={false} // İlerleme çubuğunu gösterir
-        newestOnTop={true} // Yeni mesajları en üstte gösterir
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-      />{" "}
-      {/* Toast mesajları için */}
     </Router>
   );
 };
