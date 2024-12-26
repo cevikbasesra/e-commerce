edcfa6e0b936401e373c10402e4d68ebaaaf3c80
cevikbasesra <cevikbasesra@gmail.com>
2024-12-26 17:40:49 +0300

    changed the font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#23A6F0",
+        price: "#23856D"
+      },
+      fontFamily: {
+        sans: ['Montserrat', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
